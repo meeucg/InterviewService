ce2a5412917045514b6f568ba908333657641197
meeucg <meeu.cg@gmail.com>
2026-05-05 21:27:01 +0300

    Revert "Add prompt variant conclusion guard"
    This reverts commit 760a351b5e3c4464db5222553914bc28c1c1d56b.

--- InterviewService.Application/Options/InterviewPromptingOptions.cs
+++ InterviewService.Application/Options/InterviewPromptingOptions.cs
@@ -5,23 +5,6 @@ public sealed class InterviewPromptingOptions
     public const string SectionName = "InterviewPrompting";
 
     public string Variant { get; init; } = PromptVariants.Default;
-
-    public int? MinimumDynamicAnswersBeforeConclusion { get; init; }
-
-    public int GetMinimumDynamicAnswersBeforeConclusion()
-    {
-        if (MinimumDynamicAnswersBeforeConclusion is { } configuredValue)
-        {
-            return Math.Max(0, configuredValue);
-        }
-
-        return Variant.Trim() switch
-        {
-            PromptVariants.Evidence3 => 3,
-            PromptVariants.Lean2 => 2,
-            _ => 0,
-        };
-    }
 }
 
 public static class PromptVariants
